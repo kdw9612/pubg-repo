4be30756914d326342b21b9688f35ba909f9ac82
kdw9612 <dwkim9612@gmail.com>
2022-04-14 01:30:07 -0400

    w

--- 66_multiprocessing.py
+++ 66_multiprocessing.py
@@ -1,2 +1,22 @@
-print('hello')
-print('world')
+# ****************************
+# Python multiprocessing
+# ****************************
+# multiprocessing = running tasks in parellel on different cpu cores, bypass GIL
+#                   multiprocessing = better for cpu bound tasks (heavy cpu usage)
+#                   multithreading = better for io bound (waiting around)
+
+from multiprocessing import Process, cpu_count
+import time
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+def main():
+    a = Process(target=counter, args=(1000000000,))
+    a.start()
+    
+    a.join()
+    
+main()
